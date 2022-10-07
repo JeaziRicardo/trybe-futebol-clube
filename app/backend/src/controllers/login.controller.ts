e05f019a40b90bf3f930dc20dd051a66b6602b00
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-10-07 19:59:44 -0300

    feat: cria o controller de login

---
+++ app/backend/src/controllers/login.controller.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+import ILogin from '../interfaces/ILogin';
+import LoginService from '../services/login.service';
+
+class LoginController {
+  private _loginService = new LoginService();
+
+  public login = async (req: Request, res: Response): Promise<Response> => {
+    const { email, password } = req.body as ILogin;
+
+    const token = await this._loginService.login({ email, password });
+
+    return res.status(200).json(token);
+  };
+}
+
+export default LoginController;
