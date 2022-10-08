9ed4cdcaed51d5134be496f1f0b36fac53f303bc
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-10-08 11:47:33 -0300

    feat: cria um schema para validar o email de login

---
+++ app/backend/src/schemas/login.schema.ts
@@ -0,0 +1,14 @@
+import * as Joi from 'joi';
+
+const message = '400|All fields must be filled';
+
+const login = Joi.object({
+  email: Joi.string().email().messages({
+    'any.required': message,
+    'string.empty': message,
+  }).required(),
+});
+
+export default {
+  login,
+};
