68d0d248eeffe1463c4ec0edb2a13a5f46b258cb
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-10-08 11:49:33 -0300

    feat: valida o login na service

--- app/backend/src/services/login.service.ts
+++ app/backend/src/services/login.service.ts
@@ -3,11 +3,14 @@ import tokenHelper from '../helpers/token.helper';
 import User from '../database/models/UserModel';
 import IToken from '../interfaces/IToken';
 import CustomError from '../erros/customErros';
+import Validate from '../middlewares/validate.middleware';
 
 class LoginService {
   private _userModel = User;
 
   async login(login: ILogin): Promise<IToken> {
+    Validate.login(login);
+
     const user = await this._userModel.findOne(
       { where: { email: login.email } },
     );
