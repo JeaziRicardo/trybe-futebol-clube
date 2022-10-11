97169ace75f1a111bdb165d500ecc5cd4fa3e021
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-10-10 22:15:51 -0300

    feat: cria service para retornar time pelo id

--- app/backend/src/services/teams.service.ts
+++ app/backend/src/services/teams.service.ts
@@ -1,3 +1,4 @@
+import CustomError from '../erros/customErros';
 import Team from '../database/models/TeamModel';
 
 class TeamsService {
@@ -8,6 +9,14 @@ class TeamsService {
 
     return teams;
   }
+
+  public getById = async (id: number): Promise<Team> => {
+    const team = await this._teamsModel.findByPk(id);
+
+    if (!team) throw new CustomError(404, 'Not found');
+
+    return team;
+  };
 }
 
 export default TeamsService;
