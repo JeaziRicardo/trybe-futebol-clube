fe33a79a78d7f7e55eac8bba68d75cbc9692cbd8
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-10-10 22:16:06 -0300

    feat: cria controller para retornar time pelo id

--- app/backend/src/controllers/teams.controller.ts
+++ app/backend/src/controllers/teams.controller.ts
@@ -9,6 +9,14 @@ class TeamsController {
 
     return res.status(200).json(teams);
   };
+
+  public getById = async (req: Request, res: Response): Promise<Response> => {
+    const { id } = req.params;
+
+    const team = await this._teamsService.getById(+id);
+
+    return res.status(200).json(team);
+  };
 }
 
 export default TeamsController;
