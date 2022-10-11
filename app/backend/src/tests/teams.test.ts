b3b879057de0da4ecd126a9755da3817a017751b
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-10-10 22:03:36 -0300

    feat: implementa os testes para rota /teams/:id

--- app/backend/src/tests/teams.test.ts
+++ app/backend/src/tests/teams.test.ts
@@ -11,7 +11,7 @@ const { expect } = chai;
 
 describe('Testa a rota de teams', () => {
 
-  describe('Testa o retorno do endpoint GET', () => {
+  describe('Testa o retorno do endpoint /teams', () => {
 
     before(async () => {
       sinon.stub(Team, 'findAll').resolves(teams as Team[]);
@@ -33,6 +33,30 @@ describe('Testa a rota de teams', () => {
       expect(response.body).to.be.an('array');
       expect(response.body).to.deep.equal(teams);
     })
+  });
 
+  describe('Testa o retorno do rota /teams/:id', () => {
+    before(async () => {
+      sinon.stub(Team, 'findByPk').resolves(teams[0] as Team);
+    });
+
+    after(async () => {
+      (Team.findByPk as sinon.SinonStub).restore();
+    });
+
+    it('Retorna o status 200', async () => {
+      const response = await chai.request(app).get('/teams/1');
+
+      chai.expect(response).to.have.status(200);
+    });
+
+    it('Retorna o time correto', async () => {
+      const response = await chai.request(app).get('/teams/1');
+
+      chai.expect(response.body).to.have.property('id').to.be.equal(1);
+      chai.expect(response.body)
+        .to.have.property('teamName')
+        .to.be.equal('Avaí/Kindermann');
+    });
   });
 });
