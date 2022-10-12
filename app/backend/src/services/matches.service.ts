ff350d98ed6e334c8cec9b60c0ae80d2cbe1d2d7
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-10-11 21:55:08 -0300

    feat: service para criar uma nova partida

--- app/backend/src/services/matches.service.ts
+++ app/backend/src/services/matches.service.ts
@@ -1,5 +1,6 @@
 import Team from '../database/models/TeamModel';
 import Match from '../database/models/MatchModel';
+import { IMatch } from '../interfaces/IMatch';
 
 class MatchesService {
   private _matchModel = Match;
@@ -22,6 +23,12 @@ class MatchesService {
     if (!inProgress) return matchesAll;
 
     return matchesInProgress;
+  }
+
+  public async create(match: IMatch): Promise<Match> {
+    const matchData = await this._matchModel.create(match);
+
+    return matchData;
   }
 }
 
