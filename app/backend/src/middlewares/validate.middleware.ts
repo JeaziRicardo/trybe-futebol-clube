87a77725f75d973fb64768c4ce241b8081bb6700
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-10-12 16:36:41 -0300

    feat: add validação para não inserir partida de time inexistente

--- app/backend/src/middlewares/validate.middleware.ts
+++ app/backend/src/middlewares/validate.middleware.ts
@@ -1,7 +1,9 @@
+import Team from '../database/models/TeamModel';
 import CustomError from '../erros/customErros';
 import ILogin from '../interfaces/ILogin';
 import Schema from '../schemas/login.schema';
 
+const NOT_FOUND = 'There is no team with such id!';
 const UNAUTHORIZED = 'It is not possible to create a match with two equal teams';
 
 const login = ({ email, password }: ILogin) => {
@@ -15,6 +17,11 @@ const login = ({ email, password }: ILogin) => {
 const match = async (teams: number[]) => {
   if (teams[0] === teams[1]) {
     throw new CustomError(401, UNAUTHORIZED);
+  }
+
+  const { count } = await Team.findAndCountAll({ where: { id: teams } });
+  if (count !== teams.length) {
+    throw new CustomError(404, NOT_FOUND);
   }
 };
 
